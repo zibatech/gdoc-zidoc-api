a2f6d1efa4191040f926f93f44b39a7957f79471
Gian Lucas Lopez Ariza <tech@ziba-col.com>
2022-08-18 14:51:08 -0500

    @feature: definidas las bases de la logica que retorna los datos, ya es interactiva en base a los parametros de tipo GET

--- blueprints/zidoc/endpoints/expedientes.py
+++ blueprints/zidoc/endpoints/expedientes.py
@@ -1,6 +1,16 @@
+from flask import request
 from .. import zidoc
+from shared.controllers.expedientes import ExpedientesController
+from shared.services.database import ZiDoc
+
+class Controller(ExpedientesController, ZiDoc):
+  pass
+
+controller = Controller()
 
 @zidoc.route('/expedientes', methods=['GET'])
 def get_expedientes():
-  pass
+  params = request.args
+  result = controller.get(request.args)
+  return result
   
--- shared/controllers/expedientes.py
+++ shared/controllers/expedientes.py
@@ -1,2 +1,27 @@
+
+# tiene que heredar de alguna base de datos
+
 class ExpedientesController:
-  pass
+
+  def get_clause(self, params):
+    params_dict = dict(params)
+    clause = ''
+    values = []
+    i = 1
+    for param in params_dict:
+      value = params_dict[param]
+      field_clause = f'{param} = %s'
+      if i < len(params_dict):
+        field_clause += ' AND '
+      clause += field_clause
+      values.append(value)
+      i += 1
+    values = tuple(values)
+    return clause, values
+
+  def get(self, params):
+    clause, values = self.get_clause(params)
+    statement = f'SELECT * FROM expedientes WHERE {clause}'
+    query = self.cursor.execute(statement, values)
+    rows = self.cursor.fetchall()
+    return rows
