ddd9ace1312e66778904942a6d1d450d9141c42e
Gian Lucas Lopez Ariza <tech@ziba-col.com>
2022-08-19 14:08:46 -0500

    @feature: agregada la validacion de peticiones usando HMAC

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from json import dumps
+from os import environ as env
 from dotenv import load_dotenv
-from flask import Flask
-from blueprints.zidoc import zidoc
+from flask import Flask, request
+from flask_hmac import Hmac
+from flask_hmac.exceptions import HmacException
 from blueprints.gdoc import gdoc
+from blueprints.zidoc import zidoc
 from core.constants import SERVER_ERROR_CODE
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['HMAC_KEY'] = env.get('HMAC_SECRET_KEY')
+hmac = Hmac(app)
 
 app.register_blueprint(zidoc, url_prefix='/zidoc')
 app.register_blueprint(gdoc, url_prefix='/gdoc')
+
+@app.before_request
+def hmac_validation():
+  try:
+    hmac.validate_signature(request)
+  except HmacException:
+    return hmac.abort()
 
 @app.errorhandler(SERVER_ERROR_CODE)
 def handle_exception(e):
